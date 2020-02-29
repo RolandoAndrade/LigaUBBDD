c7e79ef20b86ce363ed79dbe009d02c41d7dcf5c
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-02-29 17:13:41 -0400

    Primer commit

---
+++ Creates.sql
@@ -0,0 +1,10 @@
+CREATE TABLE USUARIO(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+    apellido VARCHAR(30) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    genero VARCHAR(10) NOT NULL,
+    foto VARCHAR NOT NULL,
+    correo VARCHAR NOT NULL,
+    PASSWORD VARCHAR NOT NULL
+);
