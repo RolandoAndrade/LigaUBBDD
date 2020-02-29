f1c1d77c06cdeba0c8fbddd192fc419d47ed2bcd
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-02-29 19:30:09 -0400

    Inserts de prueba y catálogos

---
+++ Inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO ROL VALUES
+(1,'Administrador', 'Usuario con todos los privilegios en el sistema'),
+(2, 'Gerente','Usuario con todos los privilegios sobre una institución deportiva'),
+(3, 'Entrenador', 'Usuario que administra un equipo'),
+(4, 'Delegado' , 'Representante en cancha que lleva el control de los partidos'),
+(5, 'Usuario', 'Usuario por defecto con privilegios mínimos');
