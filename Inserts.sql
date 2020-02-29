2021320a89db158c70c9025d7d316438391c9cad
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-02-29 19:49:29 -0400

    Agregadas algunas universidades

--- Inserts.sql
+++ Inserts.sql
@@ -4,3 +4,13 @@ INSERT INTO ROL VALUES
 (3, 'Entrenador', 'Usuario que administra un equipo'),
 (4, 'Delegado' , 'Representante en cancha que lleva el control de los partidos'),
 (5, 'Usuario', 'Usuario por defecto con privilegios mínimos');
+
+
+INSERT INTO UNIVERSIDAD VALUES
+(1, 'Universidad Católica Andrés Bello', 'https://upload.wikimedia.org/wikipedia/commons/0/07/Escudo_UCAB.jpg'),
+(2, 'Universidad Central de Venezuela', 'https://upload.wikimedia.org/wikipedia/commons/9/9c/UCV.png'),
+(3, 'Universidad Simón Bolívar', 'https://thumbnail.imgbin.com/12/14/10/imgbin-sim-n-bol-var-university-central-university-of-venezuela-andr-s-bello-catholic-university-universidad-metropolitana-cebolla-gvbQKMRX4bkPLsD3jZDRGwggb_t.jpg'),
+(4, 'Universidad Metropolitana', 'https://sites.google.com/site/equipogav06/_/rsrc/1363960300121/config/customLogo.gif'),
+(5, 'Universidad Santa María', 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Escudo_USM.png'),
+(6, 'Universidad Católica Santa Rosa', 'https://pbs.twimg.com/profile_images/581221276129320962/6hC8sJNb_400x400.png'),
+(7, 'Universidad Alejandro de Humboldt', 'https://upload.wikimedia.org/wikipedia/en/0/02/UAH_LOGO_.jpg');
