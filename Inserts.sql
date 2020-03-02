0e36779f9997f37d9e1817ed0343b27b6412c8ee
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-03-02 18:13:23 -0400

    Agregadas universidades

--- Inserts.sql
+++ Inserts.sql
@@ -11,6 +11,8 @@ INSERT INTO UNIVERSIDAD VALUES
 (2, 'Universidad Central de Venezuela', 'https://upload.wikimedia.org/wikipedia/commons/9/9c/UCV.png'),
 (3, 'Universidad Simón Bolívar', 'https://thumbnail.imgbin.com/12/14/10/imgbin-sim-n-bol-var-university-central-university-of-venezuela-andr-s-bello-catholic-university-universidad-metropolitana-cebolla-gvbQKMRX4bkPLsD3jZDRGwggb_t.jpg'),
 (4, 'Universidad Metropolitana', 'https://sites.google.com/site/equipogav06/_/rsrc/1363960300121/config/customLogo.gif'),
-(5, 'Universidad Santa María', 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Escudo_USM.png'),
-(6, 'Universidad Católica Santa Rosa', 'https://pbs.twimg.com/profile_images/581221276129320962/6hC8sJNb_400x400.png'),
-(7, 'Universidad Alejandro de Humboldt', 'https://upload.wikimedia.org/wikipedia/en/0/02/UAH_LOGO_.jpg');
+(5, 'Universidad Alejandro de Humboldt', 'https://upload.wikimedia.org/wikipedia/en/0/02/UAH_LOGO_.jpg'),
+(6, 'Universidad Moteávila','https://pbs.twimg.com/profile_images/952741087164198912/C-_8jcZJ_400x400.jpg'),
+(7, 'Universidad Militar Bolivariana de Venezuela','https://es.wikipedia.org/wiki/Archivo:UMBV_seal.png'),
+(8, 'Universidad Bolivariana de Venezuela', 'https://3.bp.blogspot.com/-DsTOaUq5h2A/T2cnZww_pZI/AAAAAAAAACQ/O4rdrK2JmcU/s1600/ubv.jpg'),
+(9, 'Universidad Nacional Experimental Fuerzas Armadas','https://i.pinimg.com/originals/7f/08/31/7f083170e1f7fc43aefaf1352c1ffd95.jpg');
